25417654d75b905dba5fb828e6d023530e8b2c62
steviewarbs <steviewarbs1@hotmail.co.uk>
2020-10-23 10:20:27 +0100

    added basic README

---
+++ myapp/README.md
@@ -0,0 +1,20 @@
+Project Title
+
+Baseline Node JS server to work around common CORS issues
+
+Getting Started
+
+Simply fork or copy the files into a local directory, be sure to npm init.
+
+Prerequisites
+
+Install npm install express --save
+Follow the guid here for a tutorial - https://expressjs.com/en/starter/installing.html 
+
+
+Built With
+
+Language:Node.js and Javascript
+
+Authors
+Steve Warburton
--- myapp/app.js
+++ myapp/app.js
@@ -11,12 +11,10 @@ app.use((req, res, next) => {
 
 app.get('/rest/sonar', (req, res) => {
   request(
-    { url: 'XXXXXXXXXXXXXXX',
-
-    //Pull headers from postman or client
+    { url: 'http://sonarqube.gcp.build.matillion.com:9000/sonar/api/measures/component?component=emerald&metricKeys=ncloc_language_distribution',
     
       headers : {
-      "Authorization" : "XXXXXXXXXXXXX"
+      "Authorization" : "Basic c29uYXIucXViZTpCVlRzVnBac3BLWTNBSDQ1WXQ="
     }},
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
