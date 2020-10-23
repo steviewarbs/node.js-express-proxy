59e96b0edac0ce7ebd51553c4e6bf89a1f283ba4
steviewarbs <steviewarbs1@hotmail.co.uk>
2020-10-23 10:09:36 +0100

    node-proxy-baseline

---
+++ myapp/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
+app.get('/rest/sonar', (req, res) => {
+  request(
+    { url: 'XXXXXXXXXXXXXXX',
+
+    //Pull headers from postman or client
+    
+      headers : {
+      "Authorization" : "XXXXXXXXXXXXX"
+    }},
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return "Please try again";
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
